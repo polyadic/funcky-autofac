77fbd744898932dbb1e159d9960af0a2ee054519
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-10-02 14:49:25 +0200

    Implement resolving of an Option

---
+++ Funcky.Autofac.Test/ContainerBuilderExtensionsTest.cs
@@ -0,0 +1,54 @@
+using Autofac;
+using Funcky.Monads;
+using Funcky.Xunit;
+using Xunit;
+
+namespace Funcky.Autofac.Test
+{
+    public sealed class ContainerBuilderExtensionsTest
+    {
+        private interface ILogger
+        {
+        }
+
+        [Fact]
+        public void ResolvingAnOptionWhenTypeIsNotRegisteredReturnsNone()
+        {
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterOption();
+            using var container = containerBuilder.Build();
+            FunctionalAssert.IsNone(container.Resolve<Option<ILogger>>());
+        }
+
+        [Fact]
+        public void ResolvingAnOptionWhenTypeIsRegisteredReturnsInstance()
+        {
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterOption();
+            containerBuilder.RegisterType<NullLogger>().AsImplementedInterfaces();
+            using var container = containerBuilder.Build();
+            var logger = FunctionalAssert.IsSome(container.Resolve<Option<ILogger>>());
+            Assert.True(logger is NullLogger);
+        }
+
+        [Fact]
+        public void ResolvingAnOptionWhenMultipleTypesForSameServiceAreRegisteredReturnsDefaultService()
+        {
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterOption();
+            containerBuilder.RegisterType<NullLogger>().AsImplementedInterfaces();
+            containerBuilder.RegisterType<StandardOutputLogger>().AsImplementedInterfaces();
+            using var container = containerBuilder.Build();
+            var logger = FunctionalAssert.IsSome(container.Resolve<Option<ILogger>>());
+            Assert.True(logger is StandardOutputLogger);
+        }
+
+        private class NullLogger : ILogger
+        {
+        }
+
+        private class StandardOutputLogger : ILogger
+        {
+        }
+    }
+}
